1f10cb5c47be5a07e483e6507ceb81fc6c459ab5
pjmicolet <p.micolet@gmail.com>
2021-01-25 20:39:20 +0000

    add text bar

--- graph_example.py
+++ graph_example.py
@@ -1,4 +1,5 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QLineEdit, QVBoxLayout, QWidget
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import yfinance as yf
@@ -10,19 +11,38 @@ def getData(symbol):
     data = yf.Ticker(symbol)
     hist = data.history(period="max")
     return hist
+
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        self.graphWidget = pg.PlotWidget()
-        self.setCentralWidget(self.graphWidget)
 
+        self.graphWidget = pg.PlotWidget()
+
+        self.textbox = QLineEdit(self)
+        self.textbox.resize(280, 40)
+        self.textbox.returnPressed.connect(self.on_pushButtonOk_clicked)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.graphWidget)
+        layout.addWidget(self.textbox)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+    
+        self.setCentralWidget(widget)
 
     def plotData(self, symbol):
         stock = getData(symbol)    
 
         time = pd.to_datetime( stock.index )
+        self.graphWidget.clear()
         self.graphWidget.plot([ x.timestamp() for x in stock.index ], stock["Close"])
+
+    @QtCore.pyqtSlot()
+    def on_pushButtonOk_clicked(self):
+        newStock = self.textbox.text()
+        self.plotData(newStock)
 
 
 def main():
